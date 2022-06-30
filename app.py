3b8597a400d49c55a4b4862bdec25d7b7563d8b7
evankahenya <evankahenya@gmail.com>
2022-06-30 16:09:33 +0300

    flask migrate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 ######################################
 #### SET UP OUR SQLite DATABASE #####
@@ -15,6 +16,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+Migrate(app, db)
 
 
 #####################################
